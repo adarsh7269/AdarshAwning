014d96712adb318d97b5c82d046f352bc5af4d93
adarsh7269 <as195058@gmail.com>
2026-03-09 11:21:11 +0530

    add backend and docker configuration

---
+++ backend/src/main/java/com/example/hello/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.hello;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = { "http://localhost:5173", "http://localhost" })
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Map<String, String> hello() {
+        return Map.of("message", "Hello, Adarsh!");
+    }
+}
